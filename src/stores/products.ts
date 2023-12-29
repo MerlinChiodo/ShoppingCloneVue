e0e9c374f8eec656b91835d309dbf701fa0aa072
Paul Merlin Nagel <p-merlin@web.de>
2023-12-29 12:28:53 +0100

    first functioning ali express clone with bootstrap

--- src/stores/products.ts
+++ src/stores/products.ts
@@ -1,47 +1,41 @@
 import { defineStore } from 'pinia'
 
 export interface Product {
-  id: number;
-  title: string;
-  description: string;
+  id: number
+  title: string
+  description: string
+  thumbnail: string
   // other properties
 }
 
 interface ProductsList {
-  products: Product[];
+  products: Product[]
 }
 
-
-
-export const useProductsStore = defineStore( 'products', {
+export const useProductsStore = defineStore('products', {
   state: (): ProductsList => ({
     products: []
   }),
   actions: {
     async fetchProductsfromDB() {
-      const fetchURL = 'https://dummyjson.com/products/';
+      const fetchURL = 'https://dummyjson.com/products/'
       try {
-        const response = await fetch(fetchURL);
-        if(!response.ok) {
+        const response = await fetch(fetchURL)
+        if (!response.ok) {
           throw new Error(`Failed to fetch json from ${fetchURL}`)
         }
-        const json: any = await response.json();
+        const json: any = await response.json()
 
         // Hier sicherstellen, dass jedes Element dem Typ Product entspricht
         if (Array.isArray(json.products)) {
-          this.products = json.products as Product[];
-        }else{
-          throw new Error("The json.products is not in Array format.")
+          this.products = json.products as Product[]
+        } else {
+          throw new Error('The json.products is not in Array format.')
         }
-        
-        
       } catch (error) {
-        console.error("error fetching products", error)
+        console.error('error fetching products', error)
         throw error
       }
-
-      
     }
   }
-
-} )
+})
--- src/views/catalog-view.vue
+++ src/views/catalog-view.vue
@@ -1,32 +1,89 @@
 <template>
-  <h1>Catalog</h1>
+  <NavBarVue />
   <div v-if="productsStore && productsStore.products">
-    <ul>
-    <li 
-      class="product"
-      v-for="product in productsStore.products" :key="product.id"
-    >
-    <strong> {{ product.title }}</strong>
-      <!-- Hier können Sie product-Daten verwenden -->
-  </li>
-</ul>
+    <div class="row">
+      <div class="col-sm-4 best2products rounded-4">
+        <h2 class="red">Wer zuerst kommt, -50%</h2>
+        <h5>Gratisversand, Lieferung in 10 Tagen</h5>
+        <!-- Untereinander 2 Produkte Links -->
+        <div
+          class="product-col"
+          v-for="product in productsStore.products.slice(0, 2)"
+          :key="product.id"
+        >
+          <img
+            class="rounded-4 product_image"
+            :src="product.thumbnail"
+            alt="Produktbild konnte nicht geladen werden"
+          />
+        </div>
+      </div>
+
+      <div class="col-sm-8 best3products rounded-4">
+        <!-- Nebeneinander 3 Produkte Rechts -->
+        <h2>Produkte die du lieben könntest</h2>
+        <h5>Gratisversand, Lieferung in 10 Tagen</h5>
+        <div class="row">
+          <div
+            class="col-sm-4"
+            v-for="product in productsStore.products.slice(3, 9)"
+            :key="product.id"
+          >
+            <img
+              class="rounded-4 product_image"
+              :src="product.thumbnail"
+              alt="Produktbild konnte nicht geladen werden"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { onMounted } from "vue";
-import { useProductsStore } from "../stores/products";
+import { onMounted } from 'vue'
+import { useProductsStore } from '../stores/products'
+import NavBarVue from '../components/NavBar.vue'
 
+const productsStore = useProductsStore()
 
-const productsStore = useProductsStore();
+onMounted(async () => {
+  console.log('MOUNTED >>>>>>>>>>')
 
-onMounted( async () => {
-  console.log("MOUNTED >>>>>>>>>>")
-
-  await productsStore.fetchProductsfromDB();
-  console.log("Products fetched")
-  
+  await productsStore.fetchProductsfromDB()
+  console.log('Products fetched')
 })
-
-
 </script>
+
+<style lang="scss" scoped>
+// Überschreiben Sie die Bootstrap-Variablen
+$border-radius: 25px;
+
+@import '../node_modules/bootstrap/scss/bootstrap';
+
+.container {
+  border-radius: 25px;
+  padding: 25px;
+}
+
+.product-col {
+  min-height: 300px;
+}
+
+.product_image {
+  // Makes them scale with the parent col
+  width: 220px;
+  height: 220px;
+  object-fit: cover; /* Dies stellt sicher, dass das Bild skaliert und zugeschnitten wird, um die festgelegten Maße zu erfüllen, während das Seitenverhältnis beibehalten wird */
+}
+.best3products {
+  background-color: rgb(255, 250, 213);
+  padding: 35px;
+}
+
+.best2products {
+  background-color: #f5f5f5;
+  padding: 35px;
+}
+</style>
